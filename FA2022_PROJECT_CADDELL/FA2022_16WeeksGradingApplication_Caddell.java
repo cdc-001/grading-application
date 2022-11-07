2cb8f6be2103f87c324aa23a77164c32c3a072c0
cdc-001 <cory.caddell@gmail.com>
2022-11-07 11:18:45 -0600

    Add readScores() method and include in switch statement

--- FA2022_PROJECT_CADDELL/FA2022_16WeeksGradingApplication_Caddell.java
+++ FA2022_PROJECT_CADDELL/FA2022_16WeeksGradingApplication_Caddell.java
@@ -16,23 +16,87 @@ public class FA2022_16WeeksGradingApplication_Caddell
 			   studentID,
 			   studentName;
 		int selection;					//Menu selection
-		
+		boolean debug = true;
+				//debug = false;
+				
 		do
 		{
 			//Display menu to screen
 			System.out.println("FA2022_16WeeksGradingApplication_Caddell.java\n"
 							 + "LIST OF ASSIGNMENTS  – CORY CADDELL\n"
 							 + "--------------------------------------------------------------------\n"
-							 + "1.QUIZ\n2.HOMEWORK\n3.LAB\n4.PROJECT\n5.TEAMWORK6.TOPIC\n7.TEST\n0.EXIT\n");
+							 + "1.QUIZ\n2.HOMEWORK\n3.LAB\n4.PROJECT\n5.TEAMWORK6.TOPIC\n7.TEST\n0.EXIT");
 			
 			//Read input from user
 			Scanner keyboard = new Scanner(System.in);
 			selection = keyboard.nextInt();
 			
-			
+			switch (selection)
+			{
+				case 0:
+					break;
+				case 1:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				case 2:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				case 3:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				case 4:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				case 5:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				case 6:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				case 7:
+					readScores(selection, assignmentNames, assignmentScores, keyboard);
+					break;
+				default:
+					System.out.println("Invalid selection.");
+					break;
+			}
 			
 		}while(selection !=0);
 		
-		System.out.println("Thank you! Have a nice day!")
+		if (debug)
+		{
+			for (int row = 0; row < assignmentScores.length; row++)
+			{
+				for (int col = 0; col < assignmentScores[row].length; col++)
+				{
+					System.out.print(assignmentScores[row][col]);
+				}
+				System.out.println();
+			}
+		}
+		
+		System.out.println("Thank you! Have a nice day!");
+	}
+	
+	/*
+	 * Method to read and store assignment scores in the respective row of assignment array
+	 * @param s Assignment selection
+	 * @param s Assignment names
+	 * @param n Assignment score array
+	 * @param k Scanner class, keyboard object
+	 */
+	public static void readScores(int s, String[] a, int[][] n, Scanner k)
+	{
+		int max;
+		System.out.print("How many " + a[s - 1] + " scores? ");
+		max = k.nextInt();
+		
+		n[s -1] = new int[max];
+		
+		for (int i = 0; i < max; i++)
+		{
+			System.out.print("Enter your score for " + a[s - 1] + " " + (i + 1) + ": "); 
+			n[s - 1][i] = k.nextInt();
+		}
 	}
 }
